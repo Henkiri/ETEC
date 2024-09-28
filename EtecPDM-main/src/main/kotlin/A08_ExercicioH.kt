307697bf4d3b26c12c5b0067265fca30f1f31f0f
Henrique Bernardino <henriquebernardinosilva@gmail.com>
2024-09-28 09:21:16 -0300

    Update A08_ExercicioH.kt

--- EtecPDM-main/src/main/kotlin/A08_ExercicioH.kt
+++ EtecPDM-main/src/main/kotlin/A08_ExercicioH.kt
@@ -1,15 +1,13 @@
-
 // Aula 8 - Atividade pra aula de semana q vem a partir de 27/09/2024 - Henrique Bernardino Silva
 
 fun main()
 {
     anos(1)
-    qtdCaracteres("Mementos")
-    cubo(5)
-    conversorKm(30)
+    println("A palavra inserida tem ${qtdCaracteres("Mementos")} caracteres")
+    println("O cubo do número inserido é ${cubo(5)}")
+    println("O valor em milhas inserido equivale a ${conversorKm(50)} Km")
     troca("Personaaaaaaaa")
 }
-
 
 // 1. Escreva uma função que seja capaz de receber a quantidade de anos e transformar em meses, dias, horas, minutos e seggundos
 
@@ -20,7 +18,7 @@ fun anos(data: Int)
     var horas = (data * 8760)
     var minutos = (data * 525600)
     var segundos = (data * 31536000)
-    printlv("1) O numero de anos escolhido tem: $meses meses, $dias dias, $horas horas , $minutos minutos e $segundos segundos")
+    println("1) O numero de anos escolhido tem: $meses meses, $dias dias, $horas horas , $minutos minutos e $segundos segundos")
 }
 
 // 2. Escreva uma função capaz de receber um string e retornar a quantidade de caracteres
@@ -35,7 +33,6 @@ fun cubo(numero: Int) = (numero*numero*numero)
 
 fun conversorKm(milhas: Int) = milhas * 1.6
 
-
 /* 5. Escreva um programa que seja capaz de receber uma string e fazer a troca todas as letras "a" ou "A" por "x". Observando que:
     a. Não deve existir lógica dentro da função main. Main só deve ser usada como ponto de entrada.
     b. Escrever uma função para a troca de letras e impressão do valor final.
@@ -43,14 +40,10 @@ fun conversorKm(milhas: Int) = milhas * 1.6
     Exemplo: Entrada = "Programação em Kotlin" - Saída = "PROGRxMxÇxO EM KOTLIN"
  */
 
-fun troca(palavra: String)
-{
-    var palavraTrocada = palavra.replace("a" "A", "x"")
-    palavra trocada = palavra.uppercase
+fun troca(palavra: String) {
+    val palavraTrocada = palavra.replace(Regex("[aA]"), "x").uppercase()
     println("A palavra $palavra se tornou $palavraTrocada")
 }
 
 // 6. Sobre as funções criadas nos exercícios 2, 3 e 4, é possível transfomá-las em fuções de uma única linha? Se sim, trasforme-as.
 // Feito
-
-
